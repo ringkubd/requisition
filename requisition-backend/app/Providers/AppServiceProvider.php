0d83d2281697632525cd40de96b6e23bcb53b1a6
Anwar Jahid <ajr.jahid@gmail.com>
2023-09-20 11:20:10 +0600

    force https

--- requisition-backend/app/Providers/AppServiceProvider.php
+++ requisition-backend/app/Providers/AppServiceProvider.php
@@ -15,6 +15,10 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
 //        Type::addType('product_enum_type', ProductStatus::class);
+        if (!$this->app->isLocal()) {
+            //else register your services you require for production
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**
