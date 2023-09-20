de1641abe158619e353da3bdc1de1a69b814c235
Anwar Jahid <ajr.jahid@gmail.com>
2023-09-20 16:23:03 +0600

    env added in next.config.js

--- requisition-frontend/src/store/service/product/product.js
+++ requisition-frontend/src/store/service/product/product.js
@@ -7,7 +7,7 @@ export const ProductApiService = createApi({
     baseUrl: process.env.NEXT_PUBLIC_BACKEND_API_URL,
     prepareHeaders: (headers) => {
       fetch(
-        process.env.NEXT_PUBLIC_BACKEND_URL + 'sanctum/csrf-cookie',
+        process.env.NEXT_PUBLIC_BACKEND_URL + '/sanctum/csrf-cookie',
         {
           method: 'GET',
           credentials: 'include',
