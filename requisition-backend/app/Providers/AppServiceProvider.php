f59fd2bd74e948ce7f334ca6edfdbe38d54866cd
Anwar Jahid <ajr.jahid@gmail.com>
2023-09-20 11:17:52 +0600

    force https

--- requisition-backend/app/Providers/AppServiceProvider.php
+++ requisition-backend/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Dbal\ProductStatus;
 use Doctrine\DBAL\Types\Type;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if(!env('APP_DEBUG')){
+            URL::forceScheme('https');
+        }
     }
 }
