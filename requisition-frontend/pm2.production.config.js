6c2fa6e37178246d6750b02cf641d9ace6065d49
Anwar Jahid <ajr.jahid@gmail.com>
2023-10-15 11:10:37 +0600

    add pm2 config file

---
+++ requisition-frontend/pm2.production.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    apps: {
+        name: 'requisition',
+        script: 'node_modules/next/dist/bin/next',
+        args: 'start_1080',
+        instances: 2,
+        exec_mode: 'cluster',
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
+        env_prod: {
+            NODE_ENV: 'production' // NODE_ENV: 'development' || NODE_ENV: 'test'
+        }
+    }
+}
