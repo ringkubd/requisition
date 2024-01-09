3cdb6e7d811b8a5db04671cada3401c1e2331400
Anwar Jahid <ajr.jahid@gmail.com>
2024-01-09 12:26:52 +0600

    whatsapp message initiate and webhook too

--- requisition-backend/app/Http/Controllers/WhatsAppWebhookController.php
+++ requisition-backend/app/Http/Controllers/WhatsAppWebhookController.php
@@ -9,5 +9,8 @@ class WhatsAppWebhookController extends Controller
 {
     public function verify(Request $request){
         Log::error('whatsapp_response', ['response' => $request->all()]);
+        if ($request->has('hub_verify_token') && $request->hub_verify_token === env('WHATSAPP_WEBHOOK_VERIFICATION_TOKEN')){
+            return $request->hub_challenge;
+        }
     }
 }
