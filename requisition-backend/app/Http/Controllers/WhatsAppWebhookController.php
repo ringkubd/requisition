7ae9c7b8007e5b729f0fc838340d6adde5347f1f
Anwar Jahid <ajr.jahid@gmail.com>
2024-01-09 15:24:54 +0600

    whatsapp view settings done

--- requisition-backend/app/Http/Controllers/WhatsAppWebhookController.php
+++ requisition-backend/app/Http/Controllers/WhatsAppWebhookController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Log;
 class WhatsAppWebhookController extends Controller
 {
     public function verify(Request $request){
-        Log::error('whatsapp_response', ['message' => $request->entry]);
+        Log::error('whatsapp_response', ['message' => json_encode($request->entry)]);
         if ($request->isMethod('GET') && $request->has('hub_verify_token') && $request->hub_verify_token == "verification_token"){
             return $request->hub_challenge;
         }
