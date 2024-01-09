a9b4e4671709cfe559892b1dd9b8cb311a7010b0
Anwar Jahid <ajr.jahid@gmail.com>
2024-01-09 13:07:51 +0600

    route whatsapp_hook added in csrf exception

--- requisition-backend/app/Http/Controllers/WhatsAppWebhookController.php
+++ requisition-backend/app/Http/Controllers/WhatsAppWebhookController.php
@@ -9,7 +9,7 @@ class WhatsAppWebhookController extends Controller
 {
     public function verify(Request $request){
         Log::error('whatsapp_response', ['response' => $request->all()]);
-        Log::error('whatsapp_response', ['message' => $request->entry]);
+        Log::error('whatsapp_response', ['message' => json_encode($request->entry)]);
         if ($request->isMethod('GET') && $request->has('hub_verify_token') && $request->hub_verify_token == "verification_token"){
             return $request->hub_challenge;
         }
