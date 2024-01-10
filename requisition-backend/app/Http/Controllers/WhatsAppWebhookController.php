af2be25af84c42d21dcb8bda4b83a50802d6c1ce
Anwar Jahid <ajr.jahid@gmail.com>
2024-01-10 11:58:50 +0600

    webhook message implemented

--- requisition-backend/app/Http/Controllers/WhatsAppWebhookController.php
+++ requisition-backend/app/Http/Controllers/WhatsAppWebhookController.php
@@ -12,7 +12,7 @@ class WhatsAppWebhookController extends Controller
 
     public function verify(Request $request)
     {
-        Log::error('whatsapp_response', ['message' => json_encode($request->entry)]);
+        Log::error('whatsapp_response', ['message' => json_encode(collect($request->entry))]);
         if ($request->isMethod('GET') && $request->has('hub_verify_token') && $request->hub_verify_token == "verification_token") {
             return $request->hub_challenge;
         }
