40f2a40b985ea294442e4234d0e946dd0c1de8dc
Anwar Jahid <ajr.jahid@gmail.com>
2024-01-11 15:43:31 +0600

    inventory logo change

--- requisition-frontend/src/components/applicationLogo.js
+++ requisition-frontend/src/components/applicationLogo.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const ApplicationLogo = props => (
     <>
-        <Image src="/logo.svg" alt={`IsDB-BISEW`} {...props} width={100} height={100}/>
+        <Image src="/inventory.png" alt={`IsDB-BISEW`} {...props} width={100} height={100}/>
     </>
 )
 
