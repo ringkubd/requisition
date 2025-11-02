1e89a2b7ba4f386300355745735a6f285630a13a
Anwar Jahid <ajr.jahid@gmail.com>
2025-11-02 18:29:43 +0600

    d

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,6 +9,7 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                     <link
                         rel="apple-touch-icon"
                         sizes="180x180"
